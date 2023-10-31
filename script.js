cb92180c8976e44bef98f4391828dd3428168f45
Rutu kamewar <rutuj.kamewar666@gmail.com>
2023-10-31 18:55:54 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var quotes = document.querySelector(".quote-box")
+var bgImg = document.querySelector(".bg-img")
+
+
+
+async function getQuote() {
+    quotes.style.display = "block"
+    try {
+        const response = await fetch('https://api.quotable.io/random');
+        const data = await response.json();
+        const quoteElement = document.getElementById("quoteText");
+        quoteElement.textContent = `${data.content}`;
+
+        const authorSpan = document.querySelector(".author");
+        authorSpan.textContent= ` -${data.author}`;
+
+    } catch (error) {
+        console.error('Failed to fetch the quote:', error);
+    }
+}
+
